bbaa8eb88827d487d8d9bd5675f4bf3bef10b374
RomanAlvaradoHN <129681648+RomanAlvaradoHN@users.noreply.github.com>
2024-03-23 13:10:45 -0600

    Update server.py Cambio de Ip de Amazon a ip local

--- p2/server.py
+++ p2/server.py
@@ -255,7 +255,7 @@ class ServerSocket:
 if __name__ == "__main__":
 
     parametros = {
-        "server_ip": '172.31.20.154',
+        "server_ip": '127.0.0.1',
         "server_port": 9999,
     }
 
